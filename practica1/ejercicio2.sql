1e81b631b463d5e3c697d100cb491fc5b8feb1b7
Jose Ramirez <Ramirezj@alu.frlp.utn.edu.ar>
2022-09-17 14:59:58 -0300

    listo 1 y 2

--- practica1/ejercicio2.sql
+++ practica1/ejercicio2.sql
@@ -1,18 +1,19 @@
-/*INSERT INTO Anio(id_anio)
+INSERT INTO Año(id_año)
 SELECT DISTINCT ano from accidentes;
 
-INSERT INTO Comunidad_Autonoma(id_ccaa, ccaa)
-SELECT DISTINCT id_ccaa, ccaa from accidentes; */
+INSERT INTO ComunidadAutonoma(id_ccaa, ccaa)
+SELECT DISTINCT id_ccaa, ccaa from accidentes;
 
-/*INSERT INTO Tipo_Via(id_tipo_via, tipo_via)
-SELECT DISTINCT id_tipo_via, tipo_via from accidentes; */
+INSERT INTO TipoVia(id_tipo_via, tipo_via)
+SELECT DISTINCT id_tipo_via, tipo_via from accidentes;
 
-/*INSERT INTO Info_Accidente(fallecidos, heridos_hospitalizados, heridos_no_hospitalizados,
-            accidentes_mortales_30_dias, accidentes_con_victimas, Anio_id_anio, 
-            Provincia_id_provincia, Tipo_Via_id_tipo_via)
-            
-SELECT  fallecidos, heridos_hospitalizados, heridos_no_hospitalizados,
-        accidentes_mortales_30_dias, accidentes_con_victimas, ano, 
-        id_provincia, id_tipo_via
-FROM accidentes;*/
+INSERT INTO provincia(ccaa_id, id_provincia, provincia)
+SELECT DISTINCT id_ccaa, id_provincia, provincia from accidentes;
+
+INSERT INTO InfoAccidente(fallecidos, heridos_hospitalizados, heridos_no_hospitalizados,
+            accidentes_mortales_30_dias, accidentes_con_victimas, año_id, 
+            provincia_id, tipo_via_id)
+       SELECT fallecidos, heridos_hospitalizados, heridos_no_hospitalizados, accidentes_mortales_30_dias,
+       accidentes_con_victimas, ano, id_provincia, id_tipo_via
+       FROM accidentes;
 
